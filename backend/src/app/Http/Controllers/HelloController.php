44b588de90e03f6e974ea990d45f4e0262620b55
tissueMO <ts32worker@gmail.com>
2021-03-13 06:55:54 +0900

    [backend] Web-Appコンテナ疎通まで完了

---
+++ backend/src/app/Http/Controllers/HelloController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index()
+    {
+        return view('index', [
+            'message' => [
+                'Hello',
+                'World',
+                '!',
+            ],
+        ]);
+    }
+}
