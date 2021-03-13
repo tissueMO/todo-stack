715a5b67249a5d45f77ec28cb290e43db971c5f0
tissueMO <ts32worker@gmail.com>
2021-03-14 02:22:48 +0900

    #1 backend 開発環境整備

--- backend/src/app/Http/Controllers/HelloController.php
+++ backend/src/app/Http/Controllers/HelloController.php
@@ -8,6 +8,7 @@ class HelloController extends Controller
 {
     public function index()
     {
+        abort(403, 'TESTだよ');
         return view('index', [
             'message' => [
                 'Hello',
